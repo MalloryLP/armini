294685a3ecf3e5bdaa17f6d48e6661d22cf7f389
Mallory LEHUAULT-PARC <mlehuaultparc@gmail.com>
2022-03-20 10:15:56 +0100

    base program loop

---
+++ armini.ino
@@ -0,0 +1,41 @@
+#define LED1 1
+#define LED2 2
+#define PIN1 3
+
+#define ANALOG1 A0
+
+int pc = 0;
+float index = 1.2;
+
+void setup(){
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    pinMode(PIN1, INPUT_PULLUP);
+
+    pinMode(ANALOG1, INPUT);
+
+}
+
+void loop(){
+    
+    int ANALOG1_value = analogRead(ANALOG1);
+    if(ANALOG1_value < 512){
+        Serial.print(ANALOG1_value);
+        digitalWrite(LED1, LOW);
+    }else{
+        Serial.print(ANALOG1_value);
+        digitalWrite(LED1, HIGH);
+        digitalWrite(LED2, HIGH);
+    }
+
+    int PIN1_value = digitalRead(PIN1);
+    if(PIN1_value == 1){
+        digitalWrite(LED2, LOW);
+    }
+
+    while(pc < 10){
+        pc = pc + 1;
+    }
+
+
+}
