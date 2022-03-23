47be8ddf9f61794a9237969b605befff06c02a3b
Mallory <87539391+MalloryLP@users.noreply.github.com>
2022-03-23 22:49:15 +0100

    Add files via upload

--- arduinoPrettyPrinter.py
+++ arduinoPrettyPrinter.py
@@ -1,9 +1,16 @@
 class ArduinoVisitor():
     def __init__(self):
+        self.header = ""
+        self.defines = ""
+        self.declarations = ""
+        self.setup = "void setup(){\n"
+        self.loop = "void loop(){\n"
+
         self.print = ""
 
     def pp(self, program):
         self.visitProgram(program)
+        self.print = self.header + "\n"+ self.defines + "\n" + self.declarations + "\n" + self.setup + self.loop
         print("\n============ ARDUINO PRETTY PRINTER ============\n")
         print(self.print)
         print("\n========== END ARDUINO PRETTY PRINTER ==========\n")
@@ -13,10 +20,16 @@ class ArduinoVisitor():
 
     def visitProgram(self, program):
         self.visit(program.setup)
+        self.visit(program.main)
 
     def visitSetup(self, setup):
         for instance in setup.instances:
             self.visit(instance)
+        self.setup += "}\n\n"
+
+    def visitMain(self, main):
+        for declaration in main.declarations:
+            self.visit(declaration)
 
     def visitInstance(self, instance):
         CMP = instance.type
@@ -27,6 +40,8 @@ class ArduinoVisitor():
         elif CMP == "SPI":
             self.visit(instance.body)
         elif CMP == "I2C":
+            self.visit(instance.body)
+        elif CMP == "SERIAL":
             self.visit(instance.body)
 
     def visitPINType(self, type):
@@ -47,31 +62,82 @@ class ArduinoVisitor():
         for slave in type.slaves:
             self.visit(slave)
 
+    def visitSERIALType(self, type):
+        self.visit(type.baud)
+        self.setup += "\tSerial.begin(" + self.code + ");\n"
+
     def visitPINDeclaration(self, declaration):
-        self.print += "#define "
+        self.defines += "#define "
+        self.setup += "\tpinMode("
         self.visit(declaration.ident)
-        self.print += " "
+        self.defines += self.code + " "
+        self.setup += self.code + ","
         self.visit(declaration.pin)
-        self.print += "\n"
+        self.defines += self.code + "\n"
+        self.visit(declaration.level)
+        self.setup += self.code + ");\n"
 
     def visitSPIDeclaration(self, declaration):
-        self.print = "#include <SPI.h>\n" + self.print
+        self.header += "#include <SPI.h>\n"
 
     def visitI2CDeclaration(self, declaration):
-        self.print = "#include <Wire.h>\n" + self.print
+        self.header += "#include <Wire.h>\n"
+        self.setup += "\tWire.begin();\n"
 
     def visitCSDeclaration(self, cs):
-        self.print += "#define "
+        self.defines += "#define "
         self.visit(cs.ident)
-        self.print += " "
+        self.defines += self.code + " "
         self.visit(cs.pin)
-        self.print += "\n"
+        self.defines += self.code + "\n"
+
+    def visitSlaveDeclaration(self, slave):
+        self.defines += "#define "
+        self.visit(slave.ident)
+        self.defines += self.code + " "
+        self.visit(slave.addr)
+        self.defines += self.code + "\n"
+
+    def visitDeclaration(self, declaration):
+        self.declarations += declaration.type + " "
+        self.visit(declaration.body)
+        self.declarations += ";\n"
+
+    def visitIntType(self, type):
+        self.visit(type.ident)
+        self.declarations += self.code + " = "
+        self.visit(type.value)
+        self.declarations += self.code
+
+    def visitFloatType(self, type):
+        self.visit(type.ident)
+        self.declarations += self.code + " = "
+        self.visit(type.value)
+        self.declarations += self.code
+
+    def visitCharType(self, type):
+        self.visit(type.ident)
+        self.declarations += self.code + " = "
+        self.visit(type.value)
+        self.declarations += self.code
 
     def visitIdent(self, ident):
-        self.print += ident.token
+        self.code = ident.token
 
     def visitIntLit(self, int):
-        self.print += int.token
+        self.code = int.token
+
+    def visitFloatLit(self, float):
+        self.code = float.token
+
+    def visitCharLit(self, char):
+        self.code = char.token
 
     def visitAnalogLit(self, analog):
-        self.print += analog.token
+        self.code = analog.token
+
+    def visitLevelLit(self, level):
+        self.code = level.token
+
+    def visitAdressLit(self, addr):
+        self.code = addr.token
